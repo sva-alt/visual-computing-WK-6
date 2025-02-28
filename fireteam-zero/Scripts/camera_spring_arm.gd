3b1f4ffadcb6329ec8e6f63b620edbc733391e9b
dcamelos <dcamelos@unal.edu.co>
2025-02-28 15:17:49 -0500

    add character motion a mouse pointed

---
+++ fireteam-zero/Scripts/camera_spring_arm.gd
@@ -0,0 +1,14 @@
+extends SpringArm3D
+
+@export
+var player : Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position = player.position + Vector3.UP * 2
+	pass
---
+++ fireteam-zero/Scripts/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody3D
+
+@export
+var SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+@export
+var camera : Camera3D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector( "right", "left", "back", "forward")
+	var direction := (Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+	
+	var mouse_pos = get_viewport().get_mouse_position()
+	var ray_origin =  camera.project_ray_origin(mouse_pos)
+	var ray_direction = camera.project_ray_normal(mouse_pos) *  500
+	var ray_query = PhysicsRayQueryParameters3D.create(ray_origin,ray_direction)
+	
+	ray_query.collide_with_bodies = true
+	var space_states = get_world_3d().direct_space_state
+	var ray_result = space_states.intersect_ray(ray_query)
+	
+	if !ray_result.is_empty():
+		if ray_result.collider != self:
+			var target_position = ray_result.position
+			target_position.y += 1.0 
+			look_at(target_position)
