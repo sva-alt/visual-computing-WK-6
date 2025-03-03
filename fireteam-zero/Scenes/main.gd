02a00018a7775075052972625a0e38b7234a9f89
Miguel Angel Buitrago Castillo <mbuitragoc@unal.edu.co>
2025-03-03 11:01:45 -0500

    Added scoring display

--- fireteam-zero/Scenes/main.gd
+++ fireteam-zero/Scenes/main.gd
@@ -2,11 +2,26 @@ extends Node3D
 
 @onready var target = $Player
 
+var starting_nodes : int
+var current_nodes : int
+var current_wave : int
+var wave_spawned_ended
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	current_wave = 0
+	Global.current_wave = current_wave
+	starting_nodes = get_child_count()
+	current_nodes = get_child_count()
+	position_to_next_wave()
 
+func position_to_next_wave():
+	if current_nodes == starting_nodes:
+		if current_wave != 0:
+			Global.moving_to_next_wave = true
+		current_wave += 1
+		Global.current_wave = current_wave
+		print(current_wave)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- fireteam-zero/Scripts/global.gd
+++ fireteam-zero/Scripts/global.gd
@@ -4,3 +4,6 @@ var high_score = 0
 var current_score : int
 var previous_score : int
 var playerAlive = true
+ 
+var current_wave : int
+var moving_to_next_wave: bool 
